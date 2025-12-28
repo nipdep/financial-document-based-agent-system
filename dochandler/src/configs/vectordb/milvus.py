6ac7be2ea78c3944ca1fe54285b25e9c0e9bd95b
DevinDeSilva <devindesilva123@gmail.com>
2025-12-28 01:06:46 -0500

    [update] Fixes #8: vector database and test milvus

---
+++ dochandler/src/configs/vectordb/milvus.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+from dochandler.src.configs.vectordb.base import BaseVectorDbConfig
+
+class MilvusConfig(BaseVectorDbConfig):
+    def __init__(
+        self,
+        url: Optional[str] = None,
+        token: Optional[str] = None,
+        collection_name: Optional[str] = None,
+        dimensions: Optional[int] = None,
+    ):
+        """
+        Initializes a configuration class instance for MilvusDB.
+
+        :param username: username of the db server
+        :type username: Optional[str],
+        :param password: password to the db server
+        :type password: Optional[str]
+        :param app_name: Cluster app_name of the db server
+        :type app_name: Optional[str]
+        :param dbname: db name in the cluster
+        :type dbname: Optional[str]
+        :param collection_name: Default name for the collection, defaults to None
+        :type collection_name: Optional[str], optional
+        :param dimensions: number of dimension in the vector db
+        :type dir: Optional[str], optional
+        :param allow_reset: Resets the database. defaults to False
+        :type allow_reset: bool
+        :param chroma_settings: Chroma settings dict, defaults to None
+        :type chroma_settings: Optional[dict], optional
+        """
+        super().__init__(
+            url=url,
+            token=token,
+            collection_name=collection_name,
+            dimensions=dimensions,
+        )
