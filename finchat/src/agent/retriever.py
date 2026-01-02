b6f3fa57d01dcbae875287023f3a22c04a19d9ec
Oshan0721 <oshanrathnayaka53@gmail.com>
2026-01-02 01:09:54 -0500

    PR Comment Changes

--- finchat/src/agent/retriever.py
+++ finchat/src/agent/retriever.py
@@ -4,7 +4,6 @@ from cgcore.llm.base import BaseLlm
 from cgcore.embedder.base import BaseEmbedder
 from cgcore.vectordb.base import BaseVectorDB
 from cgcore.utils.utils import paragraph_list_to_str
-from QuestionAnswering.src.resources.transformations.clustering.density_base_cluster import DensityBasedCluster
 
 from icecream import ic
 
@@ -20,18 +19,6 @@ class Retriever:
     def simple_retrieve(self, prompt: str) -> str:
         input_query_embedding = self.embedder.embed(prompt)
         relevant_docs = self.db.vector_search(input_query_embedding)
-        return relevant_docs
-    
-    def extr_cluster_retrieve(self, prompt: str, cluster:DensityBasedCluster) -> str:
-        input_query_embedding = self.embedder.embed(prompt)
-        cluster.fit(force=True)
-        _ids_info = cluster.get_cluster_ids(input_query_embedding)
-        relevant_docs = []
-        for _id in _ids_info:
-            doc = self.db.find_one({"chunk_id": _id["chunk_id"]})
-            doc["retrieve_count"] = _id["count"]
-            doc["spawn_prompt"] = prompt
-            relevant_docs.append(doc)
         return relevant_docs
     
         
